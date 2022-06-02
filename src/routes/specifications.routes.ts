e89440040ad44e9564dc8f87553758026820179e
Allan Camargo <allan.cdev@hotmail.com>
2022-06-01 22:29:33 -0300

    feat: add users migrations

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
-import { listSpecificationsController } from "../modules/cars/useCases/listSpecification";
+import listSpecificationsController from "../modules/cars/useCases/listSpecification";
 
 const specificationsRoutes = Router();
 
@@ -11,7 +11,7 @@ const createSpecificationController = new CreateSpecificationController();
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 specificationsRoutes.get("/", (request, response) => {
-    return listSpecificationsController.handle(request, response);
+    return listSpecificationsController().handle(request, response);
 });
 
 export { specificationsRoutes };
